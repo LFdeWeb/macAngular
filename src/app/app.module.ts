a21e02013b93dd536fadef380d6e53587e775705
LFdeWeb <978955033@qq.com>
2018-06-19 09:55:23 +0800

    baseTrue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';  //引入的双向绑定
 
 import { AppComponent } from './app.component';//引入的根组件
-import { HeroesComponent } from './heroes/heroes.component';
+// import { HeroesComponent } from './heroes/heroes.component';
 // import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,//当前项目运行的组件
-    HeroesComponent   
+    // HeroesComponent   
   ],  
   imports: [
     BrowserModule,//引入当前模块运行依赖的其他模块
